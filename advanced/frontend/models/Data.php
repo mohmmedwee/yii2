2fa0b49c5371eb3078a209563b82d5f33bee93b2
eloustah <eloustah@gmail.com>
2017-08-23 00:27:29 +0300

    front end

---
+++ advanced/frontend/models/Data.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "data".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $email
+ * @property string $phone
+ * @property string $Date_of_birth
+ * @property string $note
+ */
+class Data extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'data';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'email', 'phone', 'Date_of_birth', 'note'], 'required'],
+            [[ 'note'], 'string', 'max' => 300],
+            [['email'],'email'],
+            [['phone'],'number'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'email' => 'Email',
+            'phone' => 'Phone',
+            'Date_of_birth' => 'Date Of Birth',
+            'note' => 'Note',
+        ];
+    }
+}
